5aa11a4c89e43100dce25ed3baac73c3ad2bb9b0
josh <jalexy12@gmail.com>
2015-07-31 01:03:40 -0400

    Got react components working...meteor 1.2 better have es6 classes included

---
+++ client/App.jsx
@@ -0,0 +1,34 @@
+App = class App extends React.Component {
+
+	constructor(){
+		super()
+	}
+
+	getTasks(){
+		return [
+		      { _id: 1, text: "This is task 1" },
+		      { _id: 2, text: "This is task 2" },
+		      { _id: 3, text: "This is task 3" }
+		    ];
+	}
+
+	renderTasks(){
+		return this.getTasks().map((task) => {
+			return <Task key={task._id} task={task} />
+		})
+	}
+
+	render(){
+		return (
+			<div className="container">
+			  <header>
+			    <h1> Todo List </h1>
+			  </header>
+
+			  <ul>
+			  </ul>
+			</div>
+		)
+	}
+}
+
